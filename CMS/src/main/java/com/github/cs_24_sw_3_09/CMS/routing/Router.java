f13a8abfe3de6db5e821b85a62b18afa331b2d5d
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-11-11 10:58:03 +0100

    Changed it from a api to a restapi

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/Router.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/Router.java
@@ -3,19 +3,22 @@ package com.github.cs_24_sw_3_09.CMS.routing;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Router {
 
     @GetMapping("/")
     String home() {
-        System.out.println("Hello");
-        return "Hello World!";
+        System.out.println("/");
+        // return "Hello World!";
+        return "helloWorld";
     }
 
     @GetMapping("/test")
     String test() {
-        System.out.println("Hello");
+        System.out.println("/test");
+        // app.getModule().
         return "test123!";
     }
 }
