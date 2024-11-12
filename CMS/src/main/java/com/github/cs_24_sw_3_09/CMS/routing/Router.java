76d6145be154f832fc988f112421e5d42dd5c099
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-11-12 12:53:40 +0100

    Added api path for dds

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/Router.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/Router.java
@@ -5,8 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.cs_24_sw_3_09.CMS.modelClasses.DisplayDevice;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Router {
+
+    @GetMapping("/api/display_devices")
+    List<DisplayDevice> allDisplayDevices() {
+        System.out.println("/api/display_devices");
+
+        // TODO: Replace code below with db dds - you must create a new builder for
+        // every device
+        DisplayDevice.DisplayDeviceBuilder ddBuilder = new DisplayDevice.DisplayDeviceBuilder();
+        ddBuilder.setName("hej");
+        DisplayDevice d = ddBuilder.getDisplayDevice();
+        System.out.println(d);
+        ArrayList<DisplayDevice> ddList = new ArrayList<DisplayDevice>();
+        ddList.add(d);
+        ddBuilder.setName("2");
+        ddList.add(ddBuilder.getDisplayDevice());
+
+        return ddList;
+    }
 
     @GetMapping("/")
     String home() {
@@ -24,7 +47,7 @@ public class Router {
 
     // Example of how to make rest with varible path
     @GetMapping("/test/{testid}")
-    public String testID(@PathVariable String testid) {
+    String testID(@PathVariable String testid) {
         System.out.println("/test with ID " + testid);
         return "test your id:" + testid;
     }
