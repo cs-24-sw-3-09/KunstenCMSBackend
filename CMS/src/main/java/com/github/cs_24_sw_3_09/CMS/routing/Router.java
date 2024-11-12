25bc77bcb35e6f3b2b420c74669b9a4d0f5e496c
Aske L R Thomsen <aske.l.r.thomsen@gmail.com>
2024-11-12 13:07:21 +0100

    Able to send pictures now

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/Router.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/Router.java
@@ -3,7 +3,16 @@ package com.github.cs_24_sw_3_09.CMS.routing;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+//import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.commons.io.IOUtils;
+
+import org.springframework.http.MediaType;
+//import org.apache.tomcat.util.http.parser.MediaType;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,14 +22,17 @@ public class Router {
     String home() {
         System.out.println("/");
         // return "Hello World!";
-        return "redirect:/index.jpg";
+        return "index.jpg";
     }
 
-    @GetMapping("/test")
-    String test() {
-        System.out.println("/test");
-        // app.getModule().
-        return "test123!";
+    @GetMapping(value = "/visual_media/{mediaId}", produces = MediaType.IMAGE_PNG_VALUE)
+    @ResponseBody
+    public byte[] test(@PathVariable String mediaId) throws IOException {
+        System.out.println("ID: " + mediaId);
+
+        InputStream in = getClass()
+                .getResourceAsStream("/visual_media/" + mediaId + ".jpg");
+        return IOUtils.toByteArray(in);
     }
 
     // Example of how to make rest with varible path
