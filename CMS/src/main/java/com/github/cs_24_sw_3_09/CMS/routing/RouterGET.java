903edb362b9db9bf7d84d173b21465dc4d08cd76
Simon Stjernholm <simon@stjernholm.dev>
2024-11-13 15:07:18 +0100

    weekschedule

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/RouterGET.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/routing/RouterGET.java
@@ -15,7 +15,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RouterGET {
@@ -42,6 +44,46 @@ public class RouterGET {
         ArrayList<TimeSlot> tsList = new ArrayList<TimeSlot>();
         tsList.add(ts);
         return tsList;
+    }
+
+    @GetMapping("/api/schedule/week")
+    Map<String, List<Map<String, Object>>> getScheduleWeek() throws SQLException {
+        Connection db = HikariCPDataSource.getConnection();
+
+        String query = """
+                SELECT display_devices.id AS device_id, display_devices.name AS device_name,
+                time_slots.id AS time_slot_id, time_slots.name AS time_slot_name, time_slots.start_date, time_slots.end_date, time_slots.start_time, time_slots.end_time,
+                weekdays.monday, weekdays.tuesday, weekdays.wednesday, weekdays.thursday, weekdays.friday, weekdays.saturday, weekdays.sunday
+                FROM display_devices
+                LEFT OUTER JOIN time_slot_display_devices ON time_slot_display_devices.display_device_id = display_devices.id
+                LEFT OUTER JOIN time_slots ON time_slot_display_devices.time_slot_id = time_slots.id
+                LEFT OUTER JOIN weekdays ON time_slots.weekdays_chosen_id = weekdays.id;""";
+
+        try (PreparedStatement statement = db.prepareStatement(query)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                Map<String, List<Map<String, Object>>> resultMap = new HashMap<>();
+
+                while (resultSet.next()) {
+                    String name = resultSet.getString("device_name");
+
+                    Map<String, Object> row = new HashMap<>();
+                    int columnCount = resultSet.getMetaData().getColumnCount();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = resultSet.getMetaData().getColumnLabel(i);
+                        Object value = resultSet.getObject(columnName);
+                        row.put(columnName, value);
+                    }
+
+                    resultMap.computeIfAbsent(name, _ -> new ArrayList<>());
+                    // Only add row if time_slot_id is not null
+                    if(row.containsKey("time_slot_id") && row.get("time_slot_id") != null) resultMap.get(name).add(row);
+                }
+                return resultMap;
+            }
+        } catch (Exception e) {
+            System.err.println("Error retrieving all objects: " + e.getMessage());
+            return null;
+        }
     }
 }
 
