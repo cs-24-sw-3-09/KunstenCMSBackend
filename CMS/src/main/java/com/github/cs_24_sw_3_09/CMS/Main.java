59a73eaa63273e5d935656f30dc00e4270b6b657
Simon Stjernholm <simon@stjernholm.dev>
2024-11-18 12:02:45 +0100

    hmr fix

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/Main.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/Main.java
@@ -1,9 +1,17 @@
 package com.github.cs_24_sw_3_09.CMS;
 
+import com.github.cs_24_sw_3_09.CMS.config.SocketIOConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(CmsApplication.class, args); // Start springboot server
+        // Start springboot server
+        ApplicationContext context = SpringApplication.run(Main.class, args);
+        // Start SocketIO server
+        SocketIOConfig socketConfig = context.getBean(SocketIOConfig.class);
+        socketConfig.socketIOModule().start(); // SocketIO will automatically gracefully shutdown due to PreDestroy annotation.
     }
 }
