932ba1bf4db6b978f3deaa00530a97c1d4d95d59
Simon Stjernholm <simon@stjernholm.dev>
2024-11-25 14:35:23 +0100

    cleanup

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/controllers/AuthController.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/controllers/AuthController.java
@@ -2,13 +2,11 @@ package com.github.cs_24_sw_3_09.CMS.controllers;
 
 import com.github.cs_24_sw_3_09.CMS.model.dto.AuthRequestDto;
 import com.github.cs_24_sw_3_09.CMS.services.JwtService;
-import com.github.cs_24_sw_3_09.CMS.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
