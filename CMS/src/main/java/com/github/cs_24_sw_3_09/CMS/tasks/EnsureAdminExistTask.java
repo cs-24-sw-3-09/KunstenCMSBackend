43c51e832fb32d341d2ec97095c207ecdef5f689
Simon Stjernholm <simon@stjernholm.dev>
2024-11-26 14:29:07 +0100

    moved location

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/tasks/EnsureAdminExistTask.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/tasks/EnsureAdminExistTask.java
@@ -2,8 +2,10 @@ package com.github.cs_24_sw_3_09.CMS.tasks;
 
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.github.cs_24_sw_3_09.CMS.model.entities.UserEntity;
 import com.github.cs_24_sw_3_09.CMS.services.UserService;
 
 @Component
@@ -17,17 +19,17 @@ public class EnsureAdminExistTask {
 
     @EventListener(ApplicationStartedEvent.class)
     public void ensureAdminExist() {
-        /*if(userService.existsByAdmin()) {
+        if(userService.existsByAdmin()) {
             System.out.println("Admin already in db.s");
             return;
-        }*/
+        }
         // Create admin user
-        /*UserEntity adminEntity = UserEntity.builder()
+        UserEntity adminEntity = UserEntity.builder()
         .email("admin@kunsten.dk")
         .password(new BCryptPasswordEncoder().encode("admin123"))
         .admin(true)
         .build();
-        userService.save(adminEntity);*/
+        userService.save(adminEntity);
         System.out.println("Admin user created");
     }
     
