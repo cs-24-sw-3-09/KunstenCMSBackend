9ac810794f173af9761a783887230407afe635c1
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-11-27 14:18:33 +0100

    Made it so it only send to users that needs mail

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/services/serviceImpl/MailServiceImpl.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/services/serviceImpl/MailServiceImpl.java
@@ -1,5 +1,9 @@
 package com.github.cs_24_sw_3_09.CMS.services.serviceImpl;
 
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
@@ -9,8 +13,11 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.github.cs_24_sw_3_09.CMS.model.dto.UserDto;
 import com.github.cs_24_sw_3_09.CMS.model.entities.EmailDetailsEntity;
+import com.github.cs_24_sw_3_09.CMS.model.entities.UserEntity;
 import com.github.cs_24_sw_3_09.CMS.repositories.DisplayDeviceRepository;
+import com.github.cs_24_sw_3_09.CMS.repositories.UserRepository;
 import com.github.cs_24_sw_3_09.CMS.services.EmailService;
 
 import lombok.Setter;
@@ -23,6 +30,9 @@ public class MailServiceImpl implements EmailService {
 
     @Autowired
     private DisplayDeviceRepository displayDeviceRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Value("${EMAIL.USERNAME:}")
     private String sender;
@@ -61,12 +71,40 @@ public class MailServiceImpl implements EmailService {
         }
 
         EmailDetailsEntity email = EmailDetailsEntity.builder()
-                .recipient(receiver) // Use the injected value
                 .msgBody("<b>Screen with ID</b> " + id + " have disconnected")
                 .subject("Disconnected Screen: " + id)
                 .build();
+        //String mailResult = sendSimpleMail(email);
 
-        return sendSimpleMail(email);
+        Set<UserEntity> userList = userRepository.findUserWithNotificationsEnabled();
+        userList = filterUsersOutsidePausePeriod(userList); // filters user outside of perioed
+        for (UserEntity user : userList) {
+            email.setRecipient(user.getEmail());
+            String mailResult = sendSimpleMail(email);
+        }
+        System.out.println(userList.toString());
+        return "Worked";
+    }//
+
+    public static Set<UserEntity> filterUsersOutsidePausePeriod(Set<UserEntity> users) {
+        Date currentTime = new Date();
+
+        return users.stream()
+                .filter(user -> isOutsidePausePeriod(user, currentTime))
+                .collect(Collectors.toSet());
+    }
+
+    private static boolean isOutsidePausePeriod(UserEntity user, Date currentTime) {
+        Date start = user.getPauseNotificationStart();
+        Date end = user.getPauseNotificationEnd();
+
+        // If either start or end is null, consider it outside the pause period
+        if (start == null || end == null) {
+            return true;
+        }
+
+        // Check if the current time is outside the pause period
+        return currentTime.before(start) || currentTime.after(end);
     }
 
 }
