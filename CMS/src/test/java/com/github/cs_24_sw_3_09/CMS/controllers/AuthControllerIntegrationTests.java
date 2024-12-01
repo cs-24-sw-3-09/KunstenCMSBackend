7dd6c3ad3fb98bee9377c29a068bacc2ebe3c4e9
Simon Stjernholm <simon@stjernholm.dev>
2024-12-01 23:06:21 +0100

    works?

--- CMS/src/test/java/com/github/cs_24_sw_3_09/CMS/controllers/AuthControllerIntegrationTests.java
+++ CMS/src/test/java/com/github/cs_24_sw_3_09/CMS/controllers/AuthControllerIntegrationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
