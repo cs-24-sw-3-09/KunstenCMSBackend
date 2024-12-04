e9450adf83de5b0cca8f4401490bd6a7ab3423da
Aske L R Thomsen <aske.l.r.thomsen@gmail.com>
2024-12-04 14:06:41 +0100

    Small changes

--- CMS/src/test/java/com/github/cs_24_sw_3_09/CMS/controllers/TimeSlotControllerIntegrationTests.java
+++ CMS/src/test/java/com/github/cs_24_sw_3_09/CMS/controllers/TimeSlotControllerIntegrationTests.java
@@ -788,7 +788,6 @@ public class TimeSlotControllerIntegrationTests {
         TimeSlotEntity ts4 = TestDataUtil.createTimeSlotEntityFromData(weekdaysChosen,
             startDate, endDate, startTime, endTime);
         String json = objectMapper.writeValueAsString(ts4); 
-        //int[] associationTs4 = {4};
         json = TestDataUtil.createDDJsonWithId(json, associations);
 
         mockMvc.perform(
