c1efaec15a63f13f2d911e97ed3ced9909044e98
Lasse Moesgaard Ørgaard <lasse301003@gmail.com>
2024-12-04 14:48:59 +0100

    Update TimeSlotRepository.java

--- CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/repositories/TimeSlotRepository.java
+++ CMS/src/main/java/com/github/cs_24_sw_3_09/CMS/repositories/TimeSlotRepository.java
@@ -27,14 +27,6 @@ import org.springframework.stereotype.Repository;
 public interface TimeSlotRepository extends CrudRepository<TimeSlotEntity, Integer>,
         PagingAndSortingRepository<TimeSlotEntity, Integer> {
 
-    @Transactional
-    @Modifying
-    @Query(value = "DELETE FROM time_slot_display_device WHERE time_slot_id = :ts_id and display_device_id = :dd_id", nativeQuery = true)
-    void deleteAssociation(@Param("ts_id") Long ts_id, @Param("dd_id") Long dd_id);
-
-    @Transactional
-    @Query(value = "SELECT COUNT(*) FROM time_slot_display_device WHERE time_slot_id = :ts_id", nativeQuery = true)
-    int countAssociations(@Param("ts_id") Long ts_id);
 
     @Query("SELECT ts FROM TimeSlotEntity ts WHERE ts.displayDevices IS EMPTY")
     List<TimeSlotEntity> findTimeSlotsWithNoDisplayDevices();
